e8d924c2b13f139bf7862d48541b001ec795f3c3
Lunfir <lunfirok@gmail.com>
2018-03-12 01:45:21 +0200

    adjust db connection

--- main.cpp
+++ main.cpp
@@ -5,6 +5,25 @@ int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
+  QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+  db.setHostName("127.0.0.1");
+  db.setDatabaseName("schema");
+  db.setUserName("lunfir");
+  db.setPassword("lunfir");
+
+  bool ok = db.open();
+
+  if ( ok ) {
+    qDebug() << "Connect to database...";
+    QSqlQuery query("SELECT section_title FROM section");
+    while (query.next())
+    {
+      qWarning() << query.value(0).toString();
+    }
+  } else {
+    qDebug() << "smth going wrong: " << db.lastError();
+  }
+
   a.quit();
 
   return 0;
